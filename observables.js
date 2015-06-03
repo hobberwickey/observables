425497e65ebc0003864fa612dadac3d0e4167269
hobberwickey <hobberwickey@gmail.com>
2015-06-03 11:59:56 -0400

    Fixed problems with splice

--- observables.js
+++ observables.js
@@ -4,82 +4,87 @@ var ObservedArray = function(){
   
   var splices = [];
   function applySplices(){
-  	for (var x in observers){
-  		observers[x](splices);
-  	}
+    for (var x in observers){
+      observers[x](splices);
+    }
 
-  	splices = [];
+    splices = [];
   }
 
-  a.observe = function(){
-  	return new Observer();
+  a.observe = function(fn){
+    return new Observer(fn);
   }
 
   a.batch = function(){
-  	return new Batch();
+    return new Batch();
   }
 
   var _push = function(item){ 
-  	splices.push({ index: a.length, removed: [], addedCount: 1}) 
-  	Array.prototype.push.call(a, item);
+    splices.push({ index: a.length, removed: [], addedCount: 1}) 
+    Array.prototype.push.call(a, item);
   }  
   a.push = function(item){
-  	_push(item);
-  	applySplices();
+    _push(item);
+    applySplices();
   };
 
   var _pop = function(){ 
-  	splices.push({ index: a.length, removed: [a[a.length - 1]], addedCount: 0})
-  	Array.prototype.pop.call(a);
+    splices.push({ index: a.length, removed: [a[a.length - 1]], addedCount: 0})
+    Array.prototype.pop.call(a);
   }
   a.pop = function(){
-  	_pop();
-  	applySplices();
+    _pop();
+    applySplices();
   };
 
 
-  var _splice = function(start, end, count){ 
-  	splices.push({ index: start, removed: a.slice(start, end), addedCount: count }) 
-  	Array.prototype.splice.apply(a, arguments);
+  var _splice = function(){ 
+    console.log(arguments)
+    var start = arguments[0],
+      end = arguments[0] + (arguments[1] || 0),
+      count = arguments.length > 2 ? arguments.length - 2 : 0;
+
+    splices.push({ index: start, removed: a.slice(start, end), addedCount: count }) 
+    Array.prototype.splice.apply(a, arguments);
   };
   a.splice = function(){
-  	_splice(arguments[0], arguments[1] || 0, arguments.length > 2 ? arguments.length - 2 : 0)
-  	applySplices();
+    _splice.apply(null, arguments)
+    applySplices();
   };
 
   var _shift = function(){
-  	splices.push({ index: 0, removed: [a[0]], addedCount: 0})
-  	Array.prototype.unshift.call(a)
+    splices.push({ index: 0, removed: [a[0]], addedCount: 0})
+    Array.prototype.unshift.call(a)
   }
   a.shift = function(){
-  	_shift();
-  	applySplices();
+    _shift();
+    applySplices();
   };
 
   var _unshift = function(item){
-  	splices.push({ index: 0, removed: [], addedCount: 1});
-  	Array.prototype.shift.call(a, item);
+    splices.push({ index: 0, removed: [], addedCount: 1});
+    Array.prototype.shift.call(a, item);
   }
   a.unshift = function(item){
-  	_unshift(item);
-  	applySplices();
+    _unshift(item);
+    applySplices();
   }
 
   var Observer = function(fn){
-  	this.key = "_" + Math.random();
-  	observers[this.key] = fn;
+    this.key = "_" + Math.random();
+    observers[this.key] = fn;
 
-  	return this
+    return this
   }
 
   Observer.prototype.close = function(){
-  	delete observers[this.key];
+    delete observers[this.key];
   }
 
   var Batch = function(){ }
   Batch.prototype.push = function(item){ _push(item) }
   Batch.prototype.pop = function(){ _pop(); }
-  Batch.prototype.splice = function(){ _splice(arguments[0], arguments[1] || 0, arguments.length > 2 ? arguments.length - 2 : 0) }
+  Batch.prototype.splice = function(){ _splice.apply(null, arguments) }
   Batch.prototype.shift = function(){ _shift(); }
   Batch.prototype.unshift = function(item){ _unshift(item) }
   Batch.prototype.flush = function(){ applySplices(); }
